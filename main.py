a0799d92d2a63ba5325524ea2f9ad7b7954411ff
Егор Заболотный <luvmechade@MacBook-Air-Egor.local>
2024-09-17 14:25:47 +0700

    Update bot code with new schedule handling

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import urllib.parse
 import io
 import pandas as pd
 import re
-import hashlib
+from datetime import datetime
 
 TOKEN = '7532741882:AAENZLumHJyXYXImkHv-gflYKxejWmIyDD0'
 
@@ -15,7 +15,6 @@ bot = telebot.TeleBot(TOKEN)
 # Хранение идентификаторов сообщений
 last_message_id = None
 last_user_message_id = None
-last_file_hash = None
 
 def fetch_schedule_link():
     url = 'https://politech-nsk.ru/index.php/studentam'
@@ -35,26 +34,10 @@ def fetch_schedule_link():
 
     return "Ссылка на расписание не найдена."
 
-def calculate_file_hash(file_data):
-    # Рассчитываем хэш файла
-    hasher = hashlib.md5()
-    hasher.update(file_data)
-    return hasher.hexdigest()
-
 def download_file(url):
     response = requests.get(url)
     response.raise_for_status()
-
-    file_data = response.content
-    file_hash = calculate_file_hash(file_data)
-    
-    global last_file_hash
-    if file_hash != last_file_hash:
-        # Если хэш изменился, обновляем его
-        last_file_hash = file_hash
-        return io.BytesIO(file_data), True  # Возвращаем файл и флаг обновления
-    else:
-        return None, False  # Возвращаем None и False, если файл не изменился
+    return io.BytesIO(response.content)
 
 def delete_previous_message(chat_id, message_id):
     try:
@@ -75,12 +58,12 @@ def send_schedule(message):
     global last_message_id, last_user_message_id
     
     # Удаляем предыдущее сообщение пользователя, если оно существует
-    # if last_user_message_id:
-    #    delete_previous_message(message.chat.id, last_user_message_id)
+    if last_user_message_id:
+        delete_previous_message(message.chat.id, last_user_message_id)
 
     # Удаляем предыдущее сообщение бота, если оно существует
-    # if last_message_id:
-    #    delete_previous_message(message.chat.id, last_message_id)
+    if last_message_id:
+        delete_previous_message(message.chat.id, last_message_id)
 
     # Сохраняем ID текущего сообщения пользователя для последующего удаления
     last_user_message_id = message.message_id
@@ -91,15 +74,11 @@ def send_schedule(message):
         response_message = bot.reply_to(message, schedule_link)
         last_message_id = response_message.message_id
     else:
-        file_data, file_updated = download_file(schedule_link)
-        if file_updated:
-            file_name = f'Расписание.xlsx'
-            file_data.seek(0)
-            response_message = bot.send_document(message.chat.id, (file_name, file_data))
-            last_message_id = response_message.message_id
-        else:
-            response_message = bot.reply_to(message, "Расписание не изменилось.")
-            last_message_id = response_message.message_id
+        file_data = download_file(schedule_link)
+        file_name = f'Расписание.xlsx'
+        file_data.seek(0)
+        response_message = bot.send_document(message.chat.id, (file_name, file_data))
+        last_message_id = response_message.message_id
 
 if __name__ == '__main__':
     bot.infinity_polling()
